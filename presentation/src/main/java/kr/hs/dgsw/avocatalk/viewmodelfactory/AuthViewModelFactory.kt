ff7e7002dc5132a857de6138e6ab60a13e90f9d2
Monsteel <52942409+Monsteel@users.noreply.github.com>
2020-05-10 22:21:05 +0900

    [FIX] AuthViewModelFactory에 DeleteTokenUseCase 의존성 주입

--- presentation/src/main/java/kr/hs/dgsw/avocatalk/viewmodelfactory/AuthViewModelFactory.kt
+++ presentation/src/main/java/kr/hs/dgsw/avocatalk/viewmodelfactory/AuthViewModelFactory.kt
@@ -2,6 +2,7 @@ package kr.hs.dgsw.avocatalk.viewmodelfactory
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import kr.hs.dgsw.avocatalk.domain.usecase.DeleteTokenUseCase
 import kr.hs.dgsw.avocatalk.domain.usecase.LoginUseCase
 import kr.hs.dgsw.avocatalk.domain.usecase.RegisterUseCase
 import kr.hs.dgsw.avocatalk.domain.usecase.SendEmailUseCase
@@ -10,12 +11,14 @@ import javax.inject.Inject
 open class AuthViewModelFactory @Inject constructor(
     private val loginUseCase: LoginUseCase,
     private val registerUseCase: RegisterUseCase,
-    private val sendEmailUseCase: SendEmailUseCase
+    private val sendEmailUseCase: SendEmailUseCase,
+    private val deleteTokenUseCase: DeleteTokenUseCase
 ) : ViewModelProvider.Factory {
     override fun <T : ViewModel?> create(modelClass: Class<T>): T =
         modelClass.getConstructor(
             LoginUseCase::class.java,
             RegisterUseCase::class.java,
-            SendEmailUseCase::class.java
-        ).newInstance(loginUseCase,registerUseCase,sendEmailUseCase)
+            SendEmailUseCase::class.java,
+            DeleteTokenUseCase::class.java
+        ).newInstance(loginUseCase,registerUseCase,sendEmailUseCase,deleteTokenUseCase)
 }
