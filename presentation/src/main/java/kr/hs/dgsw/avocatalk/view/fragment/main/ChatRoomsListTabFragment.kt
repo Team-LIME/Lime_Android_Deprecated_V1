3a42d53c3ebb4cdb2447875463faf5f44014ff96
Monsteel <52942409+Monsteel@users.noreply.github.com>
2020-05-12 12:34:41 +0900

    [FIX] 불필요한 코드 삭제 및 오류 수정

--- presentation/src/main/java/kr/hs/dgsw/avocatalk/view/fragment/main/ChatRoomsListTabFragment.kt
+++ presentation/src/main/java/kr/hs/dgsw/avocatalk/view/fragment/main/ChatRoomsListTabFragment.kt
@@ -1,26 +1,6 @@
 package kr.hs.dgsw.avocatalk.view.fragment.main
 
-import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-
-import kr.hs.dgsw.avocatalk.R
 import kr.hs.dgsw.avocatalk.base.BaseFragment
 import kr.hs.dgsw.avocatalk.databinding.FragmentChatRoomsListTabBinding
 
-/**
- * A simple [Fragment] subclass.
- */
-class ChatRoomsListTabFragment : BaseFragment<FragmentChatRoomsListTabBinding>() {
-
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_chat_rooms_list_tab, container, false)
-    }
-
-}
+class ChatRoomsListTabFragment : BaseFragment<FragmentChatRoomsListTabBinding>() { }
